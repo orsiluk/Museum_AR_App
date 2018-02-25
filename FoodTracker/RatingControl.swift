d5063775fa16f9b650f0104e105c573d1ed2e49e
Orsi Lukacs <ol14474@my.bristol.ac.uk>
2018-02-25 16:28:45 +0000

    Initial Commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Orsolya Lukacs-Kisbandi on 25/02/2018.
+//  Copyright © 2018 Orsolya Lukacs-Kisbandi. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+
+}
---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Orsolya Lukacs-Kisbandi on 16/02/2018.
+//  Copyright © 2018 Orsolya Lukacs-Kisbandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // To set the ViewController object as the delegate of its nameTextField property
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        // Make sure ViewController is notified when the user picks an image.
+        
+
+    }
+    //UITextfieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    // This is how you make sure nothing bad happens if you press cancel
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss if user cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // to do something with the image or images that a user selected from the picker. In your case, you’ll take the selected image and display it in your image view.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected image but got : \(info)")
+        }
+        
+        // Display selected image at the photoview
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //This is where your implementation of UITextFieldDelegate methods comes in. You need to specify that the text field should resign its first-responder status when the user taps a button to end editing in the text field. You do this in the textFieldShouldReturn(_:) method, which gets called when the user taps Return (or in this case, Done) on the keyboard.
+
+    // Actions:
+    
+    @IBAction func SelectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        //When a user taps the image view, they should be able to choose a photo from a collection of photos, or take one of their own. Fortunately, the UIImagePickerController class has this behavior built into it.
+        
+        // Hide the keyboard - This code ensures that if the user taps the image view while typing in the text field, the keyboard is dismissed properly.
+        nameTextField.resignFirstResponder()
+        print("does know I tapped")
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        print("start picker")
+        imagePickerController.sourceType = .photoLibrary // This line of code sets the image picker controller’s source, or the place where it gets its images. The .photoLibrary option uses the simulator’s camera roll.
+        imagePickerController.delegate = self
+        print("picked")
+        present(imagePickerController, animated: true, completion: nil)
+        print("should open damn window")
+    }
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+}
+
+
+
